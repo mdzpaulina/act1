8daf976141cafe43323c43ec8ecf7c3c83308c09
mdzpau <mdzlopezp@gmail.com>
2025-05-05 23:04:35 -0600

    act 1 commit

---
+++ actividad1_filtrado_imagen.py
@@ -0,0 +1,72 @@
+#Filtrado de Imagen para Detección de Letras Centrales de una Placa de Automóvil en Python
+#Paulina Méndez López - A01644629
+import cv2
+import numpy as np
+import pytesseract
+
+# Ruta de Tesseract
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+# Cargar imagen
+placa1 = cv2.imread("placa_q.jpg")
+placa2 = cv2.imread("placa_2.jpg")
+
+#Achicar la imagen para poder leerla más facilmente
+placa2_reducida = placa2[::3, ::3]
+
+#Filtro Gaussiano
+def gaussian_blur(imagen, tamaño):
+    # Se define un tamaño de kernel
+    sig = (tamaño-1)/3
+    # Se genera una curva gausiana para obtener los valores del filtro
+    d = np.arange(-(tamaño-1)/2, (tamaño-1)/2+1).astype(int)
+    s = np.exp(-(np.power(d,2))/(2*np.power(sig,2)))
+    # Se obtienen los coheficientes del filtro mediante el producto 2D de la curva
+    kernel = np.zeros((tamaño, tamaño))
+    for yi in range(tamaño):
+        for xi in range(tamaño):
+            kernel[yi, xi]=s[yi]*s[xi]
+    kernel /= kernel.sum()
+    imagen_filtrada = cv2.filter2D(imagen, -1, kernel)
+    
+    return imagen_filtrada
+
+
+#Se suaviza la imagen para reducir ruido y no se tomen otros caracteres innecesarios en el OCR
+imagen_suavizada1 = gaussian_blur(placa1, 28)
+imagen_suavizada2 = gaussian_blur(placa2_reducida, 15)
+
+# Se convierte la imagen de la placa 1 a HSV para facilitar la deteccion de colores oscuros
+hsv = cv2.cvtColor(imagen_suavizada1, cv2.COLOR_BGR2HSV)
+
+#Definir los rangos para la mascara
+lower = np.array([0, 0, 0])
+upper = np.array([180, 0, 114])
+
+#Se crea una mascara para detectar tonos negros
+mask_hsv = cv2.inRange(hsv, lower, upper)
+black_mask_hsv = cv2.bitwise_and(hsv, hsv, mask=mask_hsv)
+
+# Convertir a escala de grises para facilitar la aplicación del umbral
+gray1 = cv2.cvtColor(black_mask_hsv, cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(imagen_suavizada2, cv2.COLOR_BGR2GRAY)
+
+# Umbral para invertir texto oscuro sobre fondo claro y que tesseract pueda leer bien las letras
+_, binaria1 = cv2.threshold(gray1, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+_, binaria2 = cv2.threshold(gray2, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+# OCR: restringimos a mayúsculas y caracteres si solo hay letras
+config = '--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+config2 = '--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-'
+
+#Se aplica OCR a las imagenes con umbral para obtener el texto reconocido
+texto = pytesseract.image_to_string(binaria1, config=config)
+print("Texto detectado placa 1:", texto)
+texto2 = pytesseract.image_to_string(binaria2, config=config2)
+print("Texto detectado placa 2:", texto2)
+
+#Mostramos el resultado de la imagen final 
+cv2.imshow("PlacaQ", binaria1)
+cv2.imshow("Placa2", binaria2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
